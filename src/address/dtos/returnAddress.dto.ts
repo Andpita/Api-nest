96a6d53bb233d77362316d0adeb62b32371ff0e6
Anderson 'Pita' Silva <dev.andpita@gmail.com>
2023-08-20 19:40:38 -0300

    project api-nest 5- insert citys and state in address

--- src/address/dtos/returnAddress.dto.ts
+++ src/address/dtos/returnAddress.dto.ts
@@ -1,13 +1,17 @@
+import { ReturnCityDTO } from 'src/city/dtos/returnCity.dto';
 import { AddressEntity } from '../entities/address.entity';
 
 export class ReturnAddressDTO {
   complement: string;
   numberAddress: number;
   cep: string;
+  city?: ReturnCityDTO;
 
   constructor(address: AddressEntity) {
     this.complement = address.complement;
     this.numberAddress = address.numberAddress;
     this.cep = address.cep;
+
+    this.city = new ReturnCityDTO(address.city);
   }
 }
