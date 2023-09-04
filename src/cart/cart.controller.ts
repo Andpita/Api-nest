329234e87ad36664a0442bf1c09d1d4af2c7791f
Anderson 'Pita' Silva <dev.andpita@gmail.com>
2023-09-04 05:09:06 -0300

    project api-nest 20 - cart service and cart controller part 1

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Roles } from 'src/decorator/roles.decorator';
 import { UserType } from 'src/user/enum/user-type.enum';
+import { InsertCartDTO } from './dtos/insert-cart.dto';
+import { CartEntity } from './entities/cart.entity';
+import { UserId } from 'src/decorator/userId.decorator';
+import { CartService } from './cart.service';
 
 @Roles(UserType.User)
 @Controller('cart')
-export class CartController {}
+export class CartController {
+  constructor(private readonly cartService: CartService) {}
+
+  @UsePipes(ValidationPipe)
+  @Post()
+  async insertItemCart(
+    @UserId() userId: number,
+    @Body()
+    insertItem: InsertCartDTO,
+  ): Promise<CartEntity> {
+    return this.cartService.insertItemCart(userId, insertItem);
+  }
+
+  @UsePipes(ValidationPipe)
+  @Get()
+  async getCart(@UserId() userId: number): Promise<CartEntity> {
+    return this.cartService.verifyActiveCart(userId);
+  }
+}
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CartEntity } from './entities/cart.entity';
 import { Repository } from 'typeorm';
+import { InsertCartDTO } from './dtos/insert-cart.dto';
 
 @Injectable()
 export class CartService {
@@ -9,4 +10,36 @@ export class CartService {
     @InjectRepository(CartEntity)
     private readonly cartRepository: Repository<CartEntity>,
   ) {}
+
+  async verifyActiveCart(userId: number): Promise<CartEntity> {
+    const cartActive = await this.cartRepository.findOne({
+      where: { userId },
+    });
+
+    if (!cartActive) {
+      throw new NotFoundException(`Nenhum carrinho ativo`);
+    }
+
+    return cartActive;
+  }
+
+  async createCart(userId: number): Promise<CartEntity> {
+    const newCart = this.cartRepository.save({
+      active: true,
+      userId: userId,
+    });
+
+    return newCart;
+  }
+
+  async insertItemCart(
+    userId: number,
+    insertItem: InsertCartDTO,
+  ): Promise<CartEntity> {
+    const cart = this.verifyActiveCart(userId).catch(async () => {
+      return await this.createCart(userId);
+    });
+
+    return cart;
+  }
 }
